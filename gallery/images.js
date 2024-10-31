8736ddefb3621da591332dcb9edc6b895d197f1e
brunurb <brunurb+github@gmail.com>
2024-10-31 23:32:35 +0000

    Update images.js

--- gallery/images.js
+++ gallery/images.js
@@ -1,17 +1,6 @@
 const images = [
-  "34a1af44245eadf63fbb709752107736.jpg",
-  "6UUakUxY.jpeg",
+  "8bvd4VJ7.jpeg",
+  "C8Bvd4Jb.jpeg",
   "6wYsjdb6.jpeg",
-  "7ecfbcb051acc04319b43fb205447a0f.jpg",
-  "85xqm6jb.jpeg",
-  "BaanzGIj.jpeg",
-  "bfnRm70i.jpeg",
-  "cM8Cy2U0.png",
-  "cwHZBWjq.jpeg",
-  "d3f2b6ccd34c11676fe9a84dc711de98.jpg",
-  "d8QScjPB.jpeg",
-  "d8bfXp06.jpeg",
-  "evCNf4DI.jpeg",
-  "fmVD3uCG.jpeg",
-  "fz5stHcC.jpeg"
+  "z4fGdEA3.jpeg"
 ];
