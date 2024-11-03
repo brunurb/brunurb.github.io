856e4a02f20d0362676321474946973efba14f82
brunurb <brunurb+github@gmail.com>
2024-11-03 14:55:13 +0000

    Update updateMosaic.js

--- gallery/updateMosaic.js
+++ gallery/updateMosaic.js
@@ -11,19 +11,14 @@ fs.readdir(picturesDir, (err, files) => {
         return;
     }
 
-    console.log("Files found in pictures directory:", files); // Log all files found
-
-    // Filter out image files (you can customize this based on your needs)
+    // Filter out image files
     const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif|bmp|webp)$/i.test(file));
 
-    console.log("Filtered image files:", imageFiles); // Log filtered file names
-
     // Generate the images.js file content
-    const contentArray = imageFiles.map(file => `"pictures/${file}"`).join(',\n');
-
-    // Write to images.js
+    const contentArray = imageFiles.map(file => `    "pictures/${file}"`).join(',\n');
     const imagesJsContent = `const images = [\n${contentArray}\n];\n`;
+    
+    // Write to images.js
     fs.writeFileSync(path.join(__dirname, 'images.js'), imagesJsContent, 'utf8');
-
     console.log('images.js updated successfully with image paths.');
 });
